e5ef3af74f40861a278debb8273f4e5168545bd4
Jeff Sandberg <paradox460@gmail.com>
2024-10-09 22:11:00 -0600

    v0.1.2

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Djot.MixProject do
   def project do
     [
       app: :djot,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       source_url: @source_url,
