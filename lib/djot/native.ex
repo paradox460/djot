44bf9cf1e9085a28da3d15582bfd4dd01d9a402e
Jeff Sandberg <jeff@pdx.su>
2026-06-03 22:07:26 -0600

    style: format native.ex

--- lib/djot/native.ex
+++ lib/djot/native.ex
@@ -3,16 +3,17 @@ defmodule Djot.Native do
   mode = if Mix.env() in [:dev, :test], do: :debug, else: :release
   github_url = Mix.Project.config()[:package][:links][:GitHub]
 
-  force_build = System.get_env("DJOT_BUILD") in ~w[1 true]  || Mix.env() in [:dev, :test]
+  force_build = System.get_env("DJOT_BUILD") in ~w[1 true] || Mix.env() in [:dev, :test]
 
   use RustlerPrecompiled,
-      otp_app: :djot,
-      crate: "djot_nif",
-      base_url: "#{github_url}/releases/download/v#{version}",
-      force_build: force_build,
-      mode: mode,
-      targets: Enum.uniq(["aarch64-unknown-linux-musl" | RustlerPrecompiled.Config.default_targets()]),
-      version: version
+    otp_app: :djot,
+    crate: "djot_nif",
+    base_url: "#{github_url}/releases/download/v#{version}",
+    force_build: force_build,
+    mode: mode,
+    targets:
+      Enum.uniq(["aarch64-unknown-linux-musl" | RustlerPrecompiled.Config.default_targets()]),
+    version: version
 
   def to_html(_dj, _options), do: :erlang.nif_error(:nif_not_loaded)
 end
